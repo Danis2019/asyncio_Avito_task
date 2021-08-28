e7d297e05302c8ae12531e6c051ca41d05db92e6
Danis2019 <52890857+Danis2019@users.noreply.github.com>
2021-08-28 17:57:26 +0500

    added a universal solution

--- Avito1.py
+++ Avito1.py
@@ -4,29 +4,48 @@ import asyncio
 import aiohttp
 import re
 
+def calculating(numbers):
+    N = int(len(numbers) ** 0.5)
+    list_i = list(range(0, N))
+    list_j = list(range(0, N))
+    sorted_numbers = list()
+    last_j = 0
+    last_i = 0
+    for k in range(0, N):
+        for i in list_i:
+            sorted_numbers.append(int(numbers[last_j + i * N]))
+        last_i = list_i[-1]
+        list_j.pop(0)
+        for j in list_j:
+            sorted_numbers.append(int(numbers[j + last_i * N]))
+        try:
+            last_j = list_j[-1]
+            list_i.reverse()
+            list_j.reverse()
+            list_i.pop(0)
+        except:
+            break
+    return sorted_numbers
+
 async def get_matrix(url: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             print("Status:", response.status)
             data = await response.text()
-            c = re.findall(r'[-+]?\d+', data)
-            indexes = [1, 5, 9, 13,
-                       14, 15, 16, 12,
-                       8, 4, 3, 2,
-                       6, 10, 11, 7]
-
-            def get_numb(i):
-                return int(c[i - 1])
-            output = list(map(get_numb, indexes))
-            print(output)
+            numbers = re.findall(r'[-+]?\d+', data)
+            output = calculating(numbers)
+            return output
 
 SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
-# TRAVERSAL = [
-#     10, 50, 90, 130,
-#     140, 150, 160, 120,
-#     80, 40, 30, 20,
-#     60, 100, 110, 70,
-# ]
+TRAVERSAL = [
+    10, 50, 90, 130,
+    140, 150, 160, 120,
+    80, 40, 30, 20,
+    60, 100, 110, 70,
+]
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(get_matrix(SOURCE_URL))
+def test_get_matrix():
+    assert loop.run_until_complete(get_matrix(SOURCE_URL)) == TRAVERSAL
+
+test_get_matrix()
